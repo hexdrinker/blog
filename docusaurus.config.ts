05c3ba9cfbcaa9beeaea78e751d32c424405097b
Park YuongHo <hexdrinker2020@gmail.com>
2024-06-30 15:43:18 +0900

    파일 이관

---
+++ docusaurus.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from '@docusaurus/types'
+import presets from './config/presets'
+import i18n from './config/i18n'
+import themeConfig from './config/theme'
+
+const config: Config = {
+  title: "hexdrinker's devlog",
+  tagline: '재밌게 살고 즐겁게 개발 하고 싶은 한 개발자의 이야기',
+  favicon: 'img/favicon.ico',
+  url: 'https://hexdrinker.dev',
+  baseUrl: '/',
+  organizationName: 'hexdrinker',
+  projectName: 'blog',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  i18n,
+  presets: [['classic', presets]],
+  plugins: ['docusaurus-plugin-sass'],
+  themeConfig,
+}
+
+export default config
