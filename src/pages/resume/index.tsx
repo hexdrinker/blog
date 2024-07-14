6fd4331aa9885f8f36f6eb9025572030efb813e2
Park YuongHo <hexdrinker2020@gmail.com>
2024-07-14 22:27:06 +0900

    레쥬메 페이지 파일 추가

---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@theme/Layout'
+import './styles.scss'
+
+const ResumePage = () => {
+  return (
+    <Layout>
+      <main className='ResumePage__container'>준비중입니다...</main>
+    </Layout>
+  )
+}
+
+export default ResumePage
