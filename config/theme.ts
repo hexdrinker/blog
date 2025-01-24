b1892bc2cf5a3cfbccc2f31df4e4506a370bb663
Park YuongHo <hexdrinker2020@gmail.com>
2025-01-24 22:37:20 +0900

    algolia 설정 추가

--- config/theme.ts
+++ config/theme.ts
@@ -130,12 +130,38 @@ const themeConfig: ThemeConfig = {
     theme: prismThemes.github,
     darkTheme: prismThemes.dracula,
   },
-  // algolia: {
-  //   appId: '8Y1BDL19T2',
-  //   apiKey: '4d6d54bdd34a1be5ff54b58a53596991',
-  //   indexName: 'blog',
-  //   contextualSearch: true,
-  // },
+  algolia: {
+    // The application ID provided by Algolia
+    appId: 'CVBKXKD5KS',
+
+    // Public API key: it is safe to commit it
+    apiKey: 'cba9757b0c50181739a520e03956c0bd',
+
+    indexName: 'hexdrinker',
+
+    // Optional: see doc section below
+    contextualSearch: true,
+
+    // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+    externalUrlRegex: 'external\\.com|domain\\.com',
+
+    // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+    replaceSearchResultPathname: {
+      from: '/docs/', // or as RegExp: /\/docs\//
+      to: '/',
+    },
+
+    // Optional: Algolia search parameters
+    searchParameters: {},
+
+    // Optional: path for search page that enabled by default (`false` to disable it)
+    searchPagePath: 'search',
+
+    // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+    insights: false,
+
+    //... other Algolia params
+  },
   metadata: [
     { name: 'keywords', content: 'blog, typescript, react, frontend' },
     { name: 'author', content: 'hexdrinker' },
